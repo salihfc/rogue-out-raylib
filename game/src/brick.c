f66f35aea824200aed528461d05c4e68e81d8583
salihfc <salihfc99@gmail.com>
2022-11-11 04:55:52 +0300

    ADD board

--- game/src/brick.c
+++ game/src/brick.c
@@ -1,5 +1,8 @@
 #include "raylib.h"
 
+static const int BRICK_WIDTH = 100;
+static const int BRICK_HEIGHT = 30;
+
 typedef
 struct Brick
 {
@@ -7,3 +10,10 @@ struct Brick
     Vector2 position;
 
 } Brick;
+
+
+static
+void DrawBrick(Brick* brick)
+{
+    DrawRectangleV(brick->position, (Vector2) {BRICK_WIDTH, BRICK_HEIGHT}, Fade(RED, 0.7));
+}
--- game/src/game.c
+++ game/src/game.c
@@ -1,12 +1,13 @@
 #include "utils.c"
 #include "player.c"
+#include "board.c"
 
 typedef 
 struct Game
 {
     int frame;
     Player player;
-    // Board board;
+    Board board;
 
 } Game;
 
@@ -16,6 +17,7 @@ void InitGame(Game* game)
 {
     game->frame = 0;
     ResetPlayerPosition(&(game->player));
+    InitBoard(&(game->board), (Vector2) {200, 100});
 }
 
 
@@ -25,7 +27,8 @@ void DrawGame(Game* game, float delta)
     DrawInt(game->frame / 60, (Vector2){10, 50}, 32, RED);
     Player* player = &(game->player);
     DrawPlayer(player);
-
+    Board* board = &(game->board);
+    DrawBoard(board);
 
     Vector2 mouse = GetMousePosition();
     DrawInt(mouse.x, (Vector2){10, 50 + 32 * 3}, 32, BLUE);
